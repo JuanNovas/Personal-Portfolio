7159fca063360608bb67e45b9f4412c3c31558e5
Juan Novas <juanignacionovas@gmail.com>
2024-05-04 14:30:40 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const sections = document.querySelectorAll(".section");
+  
+    function checkScroll() {
+      sections.forEach(section => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+        if (sectionTop < windowHeight * 0.55) {
+          section.classList.add("active");
+        } else {
+          section.classList.remove("active");
+        }
+      });
+    }
+  
+    window.addEventListener("scroll", checkScroll);
+    window.addEventListener("resize", checkScroll);
+  
+    checkScroll();
+  });
+  
